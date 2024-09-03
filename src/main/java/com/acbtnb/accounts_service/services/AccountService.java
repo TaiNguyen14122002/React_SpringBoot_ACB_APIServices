a1a6aee18b86fc882d02d2eff9d8618df2481aed
HuuLoc0212 <nguynlc885@gmail.com>
2024-09-04 00:29:56 +0700

    feat: Loc config run AccountsServiceApplication

--- src/main/java/com/acbtnb/accounts_service/services/AccountService.java
+++ src/main/java/com/acbtnb/accounts_service/services/AccountService.java
@@ -12,8 +12,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -24,8 +22,6 @@ public class AccountService implements IAccountService {
     private final AccountDtoConverter accountDtoConverter = new AccountDtoConverter();
     @PersistenceContext
     private EntityManager entityManager;
-    @Autowired
-    private ModelMapper modelMapper;
 
     public AccountService() {
     }
